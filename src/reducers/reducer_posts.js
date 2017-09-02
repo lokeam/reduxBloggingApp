5db2fcfb468e4157f3dcaa634ad35d1465f6d2a8
lokeam <lokeahnming@gmail.com>
2017-09-02 15:43:46 -0400

    wired up delete post

--- src/reducers/reducer_posts.js
+++ src/reducers/reducer_posts.js
@@ -1,8 +1,10 @@
-import { FETCH_POSTS } from '../actions';
+import { FETCH_POST, FETCH_POSTS } from '../actions';
 import _ from 'lodash';
 
 export default function(state =  {}, action) {
 	switch (action.type) {
+		case FETCH_POST:
+			return { ...state, [action.payload.data.id]: action.payload.data };
 		case FETCH_POSTS:
 			return _.mapKeys(action.payload.data, 'id');
 		default:
